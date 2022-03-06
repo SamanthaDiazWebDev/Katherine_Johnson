d440c39766940b5539889107d6f5e5dca25e1f47
SamanthaDiazWebDev <94150040+SamanthaDiazWebDev@users.noreply.github.com>
2022-03-06 11:27:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function toggleDarkMode() {
+    let bodyElement = document.getElementById("main-background");
+    let darkModeButtonElement = document.getElementById("dark-mode-btn");
+    
+    // if the background is black, change background to tan
+    if (bodyElement.style.background === "black") {
+        bodyElement.style.background = "#f9eae1"; 
+        darkModeButtonElement.innerHTML = "Change to Dark Mode!";
+
+    }
+   
+    // If background is tan, change background to black 
+
+    else {
+        bodyElement.style.background = "black";
+        darkModeButtonElement.innerHTML = "Change to Light Mode!";
+
+    }
+}
+
